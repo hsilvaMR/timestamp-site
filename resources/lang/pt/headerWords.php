99175c6ae70f0eba0d820f243e1feb1a75bc04ba
Carlos <csilva@mredis.com>
2021-09-24 15:59:33 +0100

    update file

---
+++ resources/lang/pt/headerWords.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'descubra' => 'Descubra como, onde e para que servem os Selos Temporais!',
+
+    'confira' => 'Confira aos seus documentos, processos, aplicações e serviços
+                a prova temporal que tanto necessitam, através dos Mstamp e Mtech. Os selos
+                temporais são uma garantia não só do conteúdo comunicado, mas também do exato
+                 momento em que ocorreu.',
+
+    'registar' => 'Registe-se'
+
+
+];
