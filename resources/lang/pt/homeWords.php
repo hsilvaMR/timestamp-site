459aa5b8119a5355f1bc411bcd80d9890a5727dc
hsilvaMR <hsilva@mredis.com>
2021-10-03 18:11:44 +0100

    update

--- resources/lang/pt/homeWords.php
+++ resources/lang/pt/homeWords.php
@@ -2,48 +2,56 @@
 
 return [
 
-        'titulo' => 'Timestamp - Controller',
+    'titulo' => 'Timestamp - Controller',
 
-        'tabelaPreco' => 'Tabela de Preços',
-        'consulteDiferentes' => 'Consulte os diferentes valores, de acordo com o tipo de de selo e com o 
+
+
+    'tabelaPreco' => 'Tabela de Preços',
+    'consulteDiferentes' => 'Consulte os diferentes valores, de acordo com o tipo de de selo e com o 
                             pacote que mais convém',
-        'dispomosAmpla' => 'dispomos de uma âmpla seleção de pacotes dos diferentes tipos de selos',
+    'dispomosAmpla' => 'dispomos de uma âmpla seleção de pacotes dos diferentes tipos de selos',
 
-        'processoCompra' => 'Processo de Compra',
+    'processoCompra' => 'Processo de Compra',
 
-        'simplesFacil' => 'Simples, fácil e acessível! A partir do registo, siga os 
+    'simplesFacil' => 'Simples, fácil e acessível! A partir do registo, siga os 
                       passos e as opções de compra preenchendo com os seus dados.',
 
-        'queremosTudo' => 'Queremos que tudo seja simples, e quando tiver dúvidas, nós ajudamos!',
+    'queremosTudo' => 'Queremos que tudo seja simples, e quando tiver dúvidas, nós ajudamos!',
 
 
-        'comoAdquiri' => 'Como Adquirir',
-        'bastaFazer' => 'Basta fazer o registo, selecionar o produto e efectuar o pagamento, simples 
+    'comoAdquiri' => 'Como Adquirir',
+    'bastaFazer' => 'Basta fazer o registo, selecionar o produto e efectuar o pagamento, simples 
                     não é? Em caso de dúvidas, fale connosco',
 
-        'criarConta' => 'Criar Conta',
+    'criarConta' => 'Criar Conta',
 
-        'QuandoOnde' => 'Quando, Onde e Como Precisar',
+    'QuandoOnde' => 'Quando, Onde e Como Precisar',
 
-        'servicosTimestamp' => 'Os serviços da Time Stamp Solutions (Selos Temporais) fazem-se 
+    'servicosTimestamp' => 'Os serviços da Time Stamp Solutions (Selos Temporais) fazem-se 
                             valer de chaves públicas e fontes de tempo credíveis para disponibilizar selos temporais 
                             fiáveis, de acordo com os padrões internacionais.',
 
-        'umServico' => 'Um serviço com o objetivo de validação cronológica para disponibilizar selos temporais para uso 
+    'umServico' => 'Um serviço com o objetivo de validação cronológica para disponibilizar selos temporais para uso 
                     como suporte às assinaturas eletrónicas, conforme Diretiva 1999/93/CE do Parlamento Europeu e do 
                     Conselho de 13 de Dezembro de 1999, do Decreto Lei 290-D/99, e demais legislação nacional 
                     aplicável.',
 
-        'contudo' => 'Contudo, os selos temporais podem também ser utilizados para qualquer outro objetivo que requeira 
+    'contudo' => 'Contudo, os selos temporais podem também ser utilizados para qualquer outro objetivo que requeira 
                  prova de que determinado(s) dado(s) existiu(ram) numa determinada data/hora.',
 
-        'osSelosTempo' => 'Os selos temporais emitidos pelos nossos serviços Time Stamp Solutions respeitam e implementam
+    'osSelosTempo' => 'Os selos temporais emitidos pelos nossos serviços Time Stamp Solutions respeitam e implementam
                       os seguintes standards:',
 
-        'eTSI' => '- ETSI TS 102.023: Electronic Signatures and Infrastructures (ESI); Policy requirements for time-stamping authorities 
+    'eTSI' => '- ETSI TS 102.023: Electronic Signatures and Infrastructures (ESI); Policy requirements for time-stamping authorities 
             - RFC 3628: Requirements for Time-Stamping Authorities',
 
-        'aPolitica' => 'A Política e Práticas de Certificação de Validação Cronológica e a Declaração de Práticas de 
+    'aPolitica' => 'A Política e Práticas de Certificação de Validação Cronológica e a Declaração de Práticas de 
                     Certificação (DPC) da Time Stamp Solutions poderão ser consultadas 
                     em: http://www.timestampsolutions.pt/pt/dpc',
+
+    // modal conteudo
+    'modatTitleTiposSelos' => 'Os nossos Selos Temporais',
+
+  'mtsa' => ''
+
 ];
--- resources/views/site/includes/modals.blade.php
+++ resources/views/site/includes/modals.blade.php
@@ -3,19 +3,47 @@
 
     <div class="modal fade modal-tipo-selos" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
         tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
+                {{-- title --}}
                 <div class="modal-header">
-                    <h5 class="modal-title title-modal-tipos-selos" id="staticBackdropLabel">Os nossos Selos Temporais</h5>
+                    <h5 class="modal-title title-modal-tipos-selos" id="staticBackdropLabel">
+                        {{ trans('homeWords.modatTitleTiposSelos') }}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                {{-- body --}}
                 <div class="modal-body">
-                    ...
+
+                    <div class="row mt-5  justify-content-center">
+                         {{-- col  Mstamp --}}
+                        <div class="col col-mstamp">
+
+                            <img src="{{ asset('img/col1.png') }}" class="img-fluid" alt="">
+
+                            <div class="d-flex flex-column  justify-content-center mb-1">
+                                <div class="p-2 bd-highlight  mt-4"> {{ trans('homeWords.tabelaPreco') }}
+                                </div>
+                                <div class="p-2 bd-highlight  font-body-box2">
+                                    {{ trans('homeWords.consulteDiferentes') }}
+                                </div>
+                                <div class="p-2 bd-highlight  font-body-box2-color ">
+                                    {{ trans('homeWords.dispomosAmpla') }}
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
+
+                {{-- footer --}}
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary">Understood</button>
                 </div>
+
             </div>
         </div>
     </div>
