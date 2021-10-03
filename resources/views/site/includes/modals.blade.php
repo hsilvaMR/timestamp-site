489eeb0d2a5bc49f5469f65f7461b7fa99340f88
hsilvaMR <hsilva@mredis.com>
2021-10-03 16:26:57 +0100

    add new file modals

---
+++ resources/views/site/includes/modals.blade.php
@@ -0,0 +1,22 @@
+@section('modal')
+    {{-- Modal os nossos selos temporais --}}
+
+    <div class="modal fade modal-tipo-selos" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title title-modal-tipos-selos" id="staticBackdropLabel">Os nossos Selos Temporais</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/site/pages/home.blade.php
+++ resources/views/site/pages/home.blade.php
@@ -62,7 +62,6 @@
                     </div>
                 </div>
 
-
             </div>
         </div>
 
@@ -70,7 +69,7 @@
         <div class="row mt-5  justify-content-center">
 
             {{-- coluna 1  tabela de precos --}}
-            <div class=" col col-1A">
+            <div class=" col col-1A" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 <img src="{{ asset('img/col1.png') }}" class="img-fluid" alt="">
 
                 <div class="d-flex flex-column  justify-content-center mb-1">
@@ -124,27 +123,8 @@
         </div>
     </div>
 
-    {{--  Modal tabela de precos ! tipos de selos  --}}
-
-    <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
+    @include('site/includes/modals')
+    @yield('modal')
 </div>
 
 @endsection
