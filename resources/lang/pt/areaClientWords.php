80f894d5a1a636e44c46576f296fd7291207f828
Carlos <csilva@mredis.com>
2021-10-18 15:56:35 +0100

    updates varios files

---
+++ resources/lang/pt/areaClientWords.php
@@ -0,0 +1,20 @@
+
+<?php
+
+return [
+
+    'titleBox' => 'A minha Conta',
+    'welcome' => 'Bem vindo à sua conta, aqui pode administrar a sua informação e os seus selos',
+    'adqSelos' => 'Adquirir Selos',
+    'nomeCliente' => 'Nome Cliente',
+    'meuSelos' => 'Os meus selos Temporais',
+    'historicoC' => 'Historico de Consumo',
+    'meusDados' => 'Os meus dados pessoais',
+    'meusVale' => 'Os meus vales de desconto',
+    'metodoPagamento' => 'Método de  Pagamento',
+    'promocao' => 'As minhas Promoções',
+    'integraAPI' => 'Integração API',
+    'seloDisponivel' => 'Selos Disponíveis',
+    'adqSelos' => 'Adquirir Selos',
+    'encerrar' => 'Encerrar a sessão',
+];
--- resources/views/plataforma/pages/platform.blade.php
+++ resources/views/plataforma/pages/platform.blade.php
@@ -1,26 +1,8 @@
-@extends('plataforma/layouts/templatePFM')
+@extends('plataforma/layouts/_template')
 
 {{-- title --}}
 @section('title', $title)
 
 @section('home')
-
-    {{-- BOX AREA DE CLIENTE --}}
-    <div class="container main-client">
-
-        <div class="row">
-
-            <div class="col col-a">
-
-                
-
-            </div>
-            <div class="col col-b">
-
-            </div>
-
-        </div>
-
-    </div>
 
 @endsection
