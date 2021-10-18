6c8cc753ba883814ad4ec19d25e612ca13c727f8
Carlos <csilva@mredis.com>
2021-10-18 16:30:13 +0100

    update files

--- resources/views/plataforma/includes/clientArea.blade.php
+++ resources/views/plataforma/includes/clientArea.blade.php
@@ -13,111 +13,139 @@
                     <label for="" id="nameClient" class="py-2">Honorio Silva</label>
                 </div>
                 {{-- adquirir selos btn --}}
-                <div class="p-2 bd-highlight btn-pay-selos text-center">
+                <div class="p-2 bd-highlight btn-pay-selos text-center mb-3 mt-3">
                     <span>{{trans('areaClientWords.adqSelos')}}</span>
                 </div>
                 {{-- items   list-group-flush --}}
-                <div class="p-2 bd-highlight">
+
+                <div class="d-flex bd-highlight border-bottom">
+                    <div class="me-auto p-2 bd-highlight   ">
+                        {{trans('areaClientWords.meuSelos')}}
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </div>
+                </div>
+
+                <div class="d-flex bd-highlight border-bottom">
+                    <div class="me-auto p-2 bd-highlight  ">
+                        {{trans('areaClientWords.historicoC')}}
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </div>
+                </div>
+
+                <div class="d-flex bd-highlight border-bottom">
+                    <div class="me-auto p-2 bd-highlight ">
+                        {{trans('areaClientWords.meusDados')}}
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </div>
+                </div>
+
+                {{--<div class="p-2 bd-highlight">
                     <ul class="list-group">
                         <li class="list-group-item border-0">
                             <span class="border-bottom">
                                 {{trans('areaClientWords.meuSelos')}}
-                                <i class="fas ms-5 fa-chevron-right"></i>
-                            </span>
+                <i class="fas ms-5 fa-chevron-right"></i>
+                </span>
 
-                        </li>
-                        <li class="list-group-item border-0">
-                            <span class="border-bottom">
-                                {{trans('areaClientWords.historicoC')}}
-                                <i class="fas ms-5 fa-chevron-right"></i>
-                            </span>
-                        </li>
-                        <li class="list-group-item border-0">
-                            <span class="border-bottom">
-                                {{trans('areaClientWords.meusDados')}}
-                                <i class="fas ms-5 fa-chevron-right"></i>
-                            </span>
-                        </li>
-                        <li class="list-group-item border-0">
-                            <span class="border-bottom">
-                                {{trans('areaClientWords.meusVale')}}
-                                <i class="fas ms-5 fa-chevron-right"></i>
-                            </span>
+                </li>
+                <li class="list-group-item border-0">
+                    <span class="border-bottom">
+                        {{trans('areaClientWords.historicoC')}}
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </span>
+                </li>
+                <li class="list-group-item border-0">
+                    <span class="border-bottom">
+                        {{trans('areaClientWords.meusDados')}}
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </span>
+                </li>
+                <li class="list-group-item border-0">
+                    <span class="border-bottom">
+                        {{trans('areaClientWords.meusVale')}}
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </span>
 
-                        </li>
-                        <li class="list-group-item border-0">
-                            <span class="border-bottom">
-                                {{trans('areaClientWords.metodoPagamento')}}
-                                <i class="fas ms-5 fa-chevron-right"></i>
-                            </span>
-                        </li>
-                        <li class="list-group-item border-0">
-                            <span class="border-bottom">
-                                {{trans('areaClientWords.encerrar')}}
-                                <i class="fas ms-5 fa-chevron-right"></i>
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-        {{-- menu direita  --}}
-        <div class="col-6 right-menu">
-
-            {{-- title  | descricao --}}
-            <div class="d-flex flex-column   text-start bd-highlight mb-3 mt-2  ">
-                {{-- title --}}
-                <div class="p-2 bd-highlight mb-3">
-                    <span class="titleName">{{trans('areaClientWords.titleBox')}}</span>
-                </div>
-                {{-- welcome descricao --}}
-                <div class="p-2 bd-highlight descricao mb-3">
-                    <span>{{trans('areaClientWords.welcome')}}</span>
-                </div>
-
-                <div class="p-2 bd-highlight  border-bottom">
-                </div>
-
-            </div>
-
-            <div class="d-flex justify-content-start">
-                {{-- btn | meus selos temporais  --}}
-                <div class="p-2 mx-2 mb-2  box-btn  bd-highlight text-center">
-                    <span>{{trans('areaClientWords.meuSelos')}}</span>
-                </div>
-                {{-- btn | historico de consumo --}}
-                <div class="p-2 mb-2   box-btn mx-2  bd-highlight text-center">
-                    <span>{{trans('areaClientWords.historicoC')}}</span>
-                </div>
-                {{-- btn | promoções --}}
-                <div class="p-2 mb-2  box-btn mx-2 bd-highlight text-center">
-                    <span>{{trans('areaClientWords.meusVale')}}</span>
-                </div>
-            </div>
-
-            <div class="d-flex  justify-content-start">
-                {{-- btn | meus dados  --}}
-                <div class="p-2 mx-2  box-btn  bd-highlight"><span>{{trans('areaClientWords.meusDados')}}</span></div>
-                {{-- btn | selos disponíveis --}}
-                <div class="p-2  mx-2 box-btn  bd-highlight"><span>{{trans('areaClientWords.seloDisponivel')}}</span>
-                </div>
-                {{-- btn | metodo pagamento --}}
-                <div class="p-2 mx-2 box-btn  bd-highlight"><span>{{trans('areaClientWords.metodoPagamento')}}</span>
-                </div>
-
-            </div>
-            {{-- btn | Integração API --}}
-
-            <div class="d-flex  mx-2 mt-2 justify-content-start">
-                <div class="p-2 box-btn  bd-highlight">
-                    <span>{{trans('areaClientWords.integraAPI')}}</span>
-                </div>
-            </div>
-
+                </li>
+                <li class="list-group-item border-0">
+                    <span class="border-bottom">
+                        {{trans('areaClientWords.metodoPagamento')}}
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </span>
+                </li>
+                <li class="list-group-item border-0">
+                    <span class="border-bottom">
+                        {{trans('areaClientWords.encerrar')}}
+                        <i class="fas ms-5 fa-chevron-right"></i>
+                    </span>
+                </li>
+                </ul>
+            </div> --}}
         </div>
 
     </div>
+    {{-- menu direita  --}}
+    <div class="col-6 right-menu">
+
+        {{-- title  | descricao --}}
+        <div class="d-flex flex-column   text-start bd-highlight mb-3 mt-2  ">
+            {{-- title --}}
+            <div class="p-2 bd-highlight mb-3">
+                <span class="titleName">{{trans('areaClientWords.titleBox')}}</span>
+            </div>
+            {{-- welcome descricao --}}
+            <div class="p-2 bd-highlight descricao mb-3">
+                <span>{{trans('areaClientWords.welcome')}}</span>
+            </div>
+
+            <div class="p-2 bd-highlight  border-bottom">
+            </div>
+
+        </div>
+
+        <div class="d-flex justify-content-start">
+            {{-- btn | meus selos temporais  --}}
+            <div class="p-2 mx-2 mb-2  box-btn  bd-highlight text-center">
+                <span>{{trans('areaClientWords.meuSelos')}}</span>
+            </div>
+            {{-- btn | historico de consumo --}}
+            <div class="p-2 mb-2   box-btn mx-2  bd-highlight text-center">
+                <span>{{trans('areaClientWords.historicoC')}}</span>
+            </div>
+            {{-- btn | promoções --}}
+            <div class="p-2 mb-2  box-btn mx-2 bd-highlight text-center">
+                <span>{{trans('areaClientWords.meusVale')}}</span>
+            </div>
+        </div>
+
+        <div class="d-flex  justify-content-start">
+            {{-- btn | meus dados  --}}
+            <div class="p-2 mx-2  box-btn  bd-highlight"><span>{{trans('areaClientWords.meusDados')}}</span></div>
+            {{-- btn | selos disponíveis --}}
+            <div class="p-2  mx-2 box-btn  bd-highlight"><span>{{trans('areaClientWords.seloDisponivel')}}</span>
+            </div>
+            {{-- btn | metodo pagamento --}}
+            <div class="p-2 mx-2 box-btn  bd-highlight"><span>{{trans('areaClientWords.metodoPagamento')}}</span>
+            </div>
+
+        </div>
+        {{-- btn | Integração API --}}
+
+        <div class="d-flex  mx-2 mt-2 justify-content-start">
+            <div class="p-2 box-btn  bd-highlight">
+                <span>{{trans('areaClientWords.integraAPI')}}</span>
+            </div>
+        </div>
+
+    </div>
+
+</div>
 
 </div>
 {{-- end box client --}}
