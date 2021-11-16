f1db352dc976fa675c02d744d3bea36ba48c910a
Carlos <csilva@mredis.com>
2021-11-16 13:00:21 +0000

    update loginPFM

--- app/Http/Controllers/Utilitario.php
+++ app/Http/Controllers/Utilitario.php
@@ -15,7 +15,9 @@ class Utilitario extends Controller
 
         // Carbon::now()->format('Y-m-d H:i:s')
         $data_time = Carbon::now();
-        $data_time->format('d-m-Y H:i:s jS F Y');
+        $data_time->format('Y-m-d H:i:s jS F Y');
+        // $dateString = '00-00-000 00:00:00';
+        //Carbon::createFromFormat('Y-m-d H:i:s', $dateString)->$data_time->format('d-m-Y H:i:s');
         return  $data_time;
     }
 
